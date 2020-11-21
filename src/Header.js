e0be3722bd66b3b58a23232b54928303679bea27
Joshu Wuebbolt <joshuawuebbolt@gmail.com>
2020-11-21 16:17:19 -0500

    Added Header

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+function Header (props){
+    return
+    <div className = "header">
+        <h1>Rice Pudding</h1>
+    </div>
+
+    
+
+}
+
+
+
+
+
+
+
+export default Header;
