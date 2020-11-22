0664bfbb350835be6a8959cb2c9a6edc4a0b1695
Joshu Wuebbolt <joshuawuebbolt@gmail.com>
2020-11-21 21:27:46 -0500

    Fixed Margin Stuff

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,11 @@ import React from "react";
 import "./Header.css"
 function Header (props){
     return <div className = "header">
-
+              
+            
               <h1>Rice Pudding</h1>
+              <h2>   Is Cool   </h2>
+              
            </div>
 
     
