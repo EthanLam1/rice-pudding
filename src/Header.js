61557e2eab6dddab1b043eb7db2d9d8c95e7ec55
Joshu Wuebbolt <joshuawuebbolt@gmail.com>
2020-11-21 21:08:10 -0500

    Please work

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Header.css"
 function Header (props){
-    return
-    <div className = "header">
-        <h1>Rice Pudding</h1>
-    </div>
+    return <div className = "header">
+
+              <h1>Rice Pudding</h1>
+           </div>
 
     
 
